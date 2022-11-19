f21965ea682691ece9198dc4d927fe331bdb277f
Danilo dos Santos <danilo.santossouza@outlook.com>
2022-11-19 12:44:35 -0300

    cria header das tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 
 import { Header } from './components/Header'
+import { Tasks } from './components/Tasks'
 import './global.css'
 
 function App() {
   return (
     <>
       <Header />
+      <Tasks />
     </>
   )
 }
